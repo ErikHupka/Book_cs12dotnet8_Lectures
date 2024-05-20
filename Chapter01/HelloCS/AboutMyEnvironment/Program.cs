fbb1a0bbc0b2cc3879d46681cf1396a0600e18a2
Hupka <erik.hupka@accenture.com>
2024-05-20 15:41:04 +0200

    Chapter 01 - Hello, C#! Welcome .NET!

---
+++ Chapter01/HelloCS/AboutMyEnvironment/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AboutMyEnvironment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.CurrentDirectory + Environment.NewLine);
+            Console.WriteLine(Environment.OSVersion.VersionString + Environment.NewLine);
+            Console.WriteLine("Namespace: {0}", typeof(Program).Namespace);
+        }
+    }
+}
---
+++ Chapter01/HelloCS/HelloCS/Program.cs
@@ -0,0 +1,6 @@
+﻿string name = typeof(Program).Namespace ?? "None";
+Console.WriteLine($"Namespace: {name}");
+
+
+Console.WriteLine("Hello, C#!");
+throw new Exception();
