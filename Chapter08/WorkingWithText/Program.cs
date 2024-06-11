49af6d71963381dd9f38bd4b42f8fbe1195d5c01
Erik Hupka <erik.hupka1@gmail.com>
2024-06-11 21:53:49 +0200

    Chapter 08 - Working with strings

--- Chapter08/WorkingWithText/Program.cs
+++ Chapter08/WorkingWithText/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string city = "London";
+WriteLine($"{city} is {city.Length} characters long.");
+WriteLine($"First char is {city[0]} and fourth is {city[3]}");
+
+string cities = "Paris,Tehran,Chennai,Sydney,New York,Medellin";
+string[] citiesArray = cities.Split(',');
+WriteLine($"There are {citiesArray.Length} items in the array:");
+foreach (string item in citiesArray)
+{
+    WriteLine($"{item}");
+}
+
+string fullName = "Alan Shore";
+int indexOfTheSpace = fullName.IndexOf(" ");
+string firstName = fullName.Substring(
+    startIndex: 0,
+    indexOfTheSpace);
+string lastName = fullName.Substring(
+    startIndex: indexOfTheSpace + 1
+    );
+WriteLine($"Origina: {fullName}");
+WriteLine($"Swapped: {lastName}, {firstName}");
