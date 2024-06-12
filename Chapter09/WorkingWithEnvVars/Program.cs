d2585b4e2df4fcd7816b8f0a8eb597e0e0425f5a
Erik Hupka <erik.hupka1@gmail.com>
2024-06-12 19:00:52 +0200

    Chapter 09 - Done

---
+++ Chapter09/WorkingWithEnvVars/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+SectionTitle("Reading all environment variables for process");
+IDictionary vars = GetEnvironmentVariables();
+DictionaryToTable(vars);
+SectionTitle("Reading all environment variables for machine");
+IDictionary varsMachine = GetEnvironmentVariables(
+  EnvironmentVariableTarget.Machine);
+DictionaryToTable(varsMachine);
+SectionTitle("Reading all environment variables for user");
+IDictionary varsUser = GetEnvironmentVariables(
+  EnvironmentVariableTarget.User);
+DictionaryToTable(varsUser);
---
+++ Chapter09/WorkingWithSerialization/Person.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization; // To use [XmlAttribute]
+namespace Packt.Shared;
+
+public class Person
+{
+    public Person() { } // A parameterless constructor is required for XML serialization.
+
+    public Person(decimal initialSalary)
+    {
+        Salary = initialSalary;
+    }
+
+    [XmlAttribute(attributeName: "fname")]
+    public string? FirstName { get; set; }
+    [XmlAttribute(attributeName: "lname")]
+    public string? LastName { get; set; }
+    [XmlAttribute(attributeName: "dob")]
+    public DateTime DateOfBirth { get; set; }
+    public HashSet<Person>? Children { get; set; }
+    protected decimal Salary { get; set; }
+}
