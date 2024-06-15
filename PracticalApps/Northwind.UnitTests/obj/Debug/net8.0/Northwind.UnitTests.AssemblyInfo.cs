70e247a161daf40d07d8b7462f230e320ec86d68
Erik Hupka <erik.hupka1@gmail.com>
2024-06-15 17:24:34 +0200

    Chapter 14

--- PracticalApps/Northwind.UnitTests/obj/Debug/net8.0/Northwind.UnitTests.AssemblyInfo.cs
+++ PracticalApps/Northwind.UnitTests/obj/Debug/net8.0/Northwind.UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Northwind.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fcf76398f90ccc32209b36d2295d3a6cf0a85a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b2615223c50a97a411c0747b292134076ab4099")]
 [assembly: System.Reflection.AssemblyProductAttribute("Northwind.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Northwind.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
